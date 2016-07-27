b1934ef047de70b81a96600c40aceebf829e12a9
seairy <seairy@gmail.com>
2016-07-27 21:54:27 +0800

    init commit

--- app/models/machine_pulse.rb
+++ app/models/machine_pulse.rb
@@ -1,5 +1,6 @@
 class MachinePulse < ActiveRecord::Base
   belongs_to :machine
+  @@test_tid = 122041
 
   def self.response params = []
     if !params[:f].blank? and !params[:t].blank? and !params[:m].blank? and !params[:g].blank? and !params[:p].blank?
@@ -8,7 +9,8 @@ class MachinePulse < ActiveRecord::Base
       battery = inner_params[:BTY].to_i
       if machine = Machine.where(serial_number: params[:m]).first
         create!(machine: machine, frame_number: params[:f], frame_type: params[:t], gprs_intensity: params[:g], out_of_stock: out_of_stock, battery: battery)
-        content = Base64.encode64('123456,35').strip
+        content = Base64.encode64("#{@@test_tid},35").strip
+        @@test_tid += 1
         "#{params[:f]},#{content.length},#{content}"
       end
     else
